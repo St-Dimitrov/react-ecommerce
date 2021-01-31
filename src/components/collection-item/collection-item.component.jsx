d0167b7d4834a62355ccbbd5dce6b7b3d313176d
St-Dimitrov <stefan.r7.dimitrov@gmail.com>
2021-01-31 23:59:17 +0200

    going to cry in the dark corner of my room

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -8,6 +8,7 @@ import "./collection-item.styles.scss";
 
 const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
+
   return (
     <div className="collection-item">
       <div
@@ -21,8 +22,7 @@ const CollectionItem = ({ item, addItem }) => {
         <span className="price">{price}</span>
       </div>
       <CustomButton onClick={() => addItem(item)} inverted>
-        {" "}
-        Add to cart{" "}
+        Add to cart
       </CustomButton>
     </div>
   );
